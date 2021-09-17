67bc4967ae612dd5eb66ceb45145a89e5af6c127
Kazuho-Shibuya <businessfive25@gmail.com>
2021-09-18 01:53:45 +0900

    ストロングパラメータを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,16 @@ class PostsController < ApplicationController
 
   end
   def create
-    render plain: params[:post].inspect
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
